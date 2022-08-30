3cf827ffc4324517e24274acf35a31f01a28177b
michaelroesinger <mroesinger@gmail.com>
2022-08-30 10:24:05 -0700

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    def name
+        @name
+    end
 
+    def name=(name)
+        @name = name
+    end
+end
+
+class Dog
+    def breed
+        @breed
+    end
+
+    def breed=(breed)
+        @breed=breed
+    end
 end
